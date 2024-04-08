7db98d8c55013edbfb68de1c8ac112d8a6a50256
denisbuarque <denisprogramadorweb@gmail.com>
2024-04-08 17:17:48 -0300

    Controller cadastro de categoria criado

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const Category = require('../models/Category');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send("Lista de Categorias");
-})
+});
+
+router.post('/store', (req, res) => {
+
+    const { title, slug } = req.body;
+
+    const data = {
+        title, 
+        slug
+    }
+
+    Category.create(data);
+
+    res.send("Categoria criada com sucesso!");
+});
 
 module.exports = router;
