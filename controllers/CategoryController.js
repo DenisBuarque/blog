ebf5562001e414f1ef0fee4573100a2e0ff3a862
denisbuarque <denisprogramadorweb@gmail.com>
2024-04-09 16:18:01 -0300

    Finalizando cadastro e lista de categorias

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const Category = require("../models/Category");
+const slugify = require("slugify");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
+
+  Category.findAll({raw: true}).then((categories) => {
+    console.log(categories);
+  });
   res.send("Lista de Categorias");
 });
 
 router.post("/store", (req, res) => {
-  const { title, slug } = req.body;
+  const { title } = req.body;
+
+  if (title === undefined) {
+    res.send("O título é obrigatório.");
+    return;
+  }
 
   const data = {
     title,
-    slug,
+    slug: slugify(title),
   };
 
-  Category.create(data);
-
-  res.send("Categoria criada com sucesso!");
+  Category.create(data).then(() => {
+    res.send("Categoria criada com sucesso!");
+  }).catch((error) => {
+    console.log(error);
+  });
 });
 
 module.exports = router;
