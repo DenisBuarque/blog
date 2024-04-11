7a02af1d3ca0fcf74efd042c0e6bd9150f9a86c6
denisbuarque <denisprogramadorweb@gmail.com>
2024-04-11 15:40:07 -0300

    função delete artigos criado

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const slugify = require('slugify');
+const slugify = require("slugify");
 
 const Article = require("../models/Article");
+const Category = require("../models/Category");
 
 router.get("/", (req, res) => {
+  //Article.findAll({ include:[{ model: Category }], raw: true })
   Article.findAll({ raw: true })
     .then((articles) => {
       res.status(200).json({ articles });
@@ -15,35 +17,54 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post('/store', (req, res) => {
-    const { title, description, category } = req.body;
+router.post("/store", (req, res) => {
+  const { title, description, category } = req.body;
 
-    if(title === undefined) {
-        res.send("Digite o título do artigo.");
-        return;
-    }
+  if (title === undefined) {
+    res.send("Digite o título do artigo.");
+    return;
+  }
 
-    if(description === undefined) {
-        res.send("Digite a descrição do artigo.");
-        return;
-    }
+  if (description === undefined) {
+    res.send("Digite a descrição do artigo.");
+    return;
+  }
 
-    if(category === undefined) {
-        res.send("Selecione uma categoria.");
-        return;
-    }
+  if (category === undefined) {
+    res.send("Selecione uma categoria.");
+    return;
+  }
 
-    const data = {
-        title, 
-        slug: slugify(title),
-        description,
-        categoryId: category
-    }
+  const data = {
+    title,
+    slug: slugify(title),
+    description,
+    categoryId: category,
+  };
 
-    Article.create(data).then(() => {
-        res.status(200).json({ message: "Artigo criado com sucesso."});
-    }).catch((error) => {
-        console.log(error);
+  Article.create(data)
+    .then(() => {
+      res.status(200).json({ message: "Artigo criado com sucesso." });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id === undefined || isNaN(id)) {
+    res.status(401).json({ message: "Id inválido!" });
+    return;
+  }
+
+  Article.destroy({ where: { id: id } })
+    .then(() => {
+      res.status(200).json({ message: "Artigo excluído com sucesso!" });
+    })
+    .catch((error) => {
+      console.log(error);
     });
 });
 
