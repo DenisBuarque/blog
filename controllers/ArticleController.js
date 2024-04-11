bca0e742e0b55b18eaa752035b46ac6246510b65
denisbuarque <denisprogramadorweb@gmail.com>
2024-04-11 15:18:39 -0300

    Listagem e cadastro de artigos na base de dados

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,8 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("Lista de Artigos");
-})
+const slugify = require('slugify');
+
+const Article = require("../models/Article");
+
+router.get("/", (req, res) => {
+  Article.findAll({ raw: true })
+    .then((articles) => {
+      res.status(200).json({ articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.post('/store', (req, res) => {
+    const { title, description, category } = req.body;
+
+    if(title === undefined) {
+        res.send("Digite o título do artigo.");
+        return;
+    }
+
+    if(description === undefined) {
+        res.send("Digite a descrição do artigo.");
+        return;
+    }
+
+    if(category === undefined) {
+        res.send("Selecione uma categoria.");
+        return;
+    }
+
+    const data = {
+        title, 
+        slug: slugify(title),
+        description,
+        categoryId: category
+    }
+
+    Article.create(data).then(() => {
+        res.status(200).json({ message: "Artigo criado com sucesso."});
+    }).catch((error) => {
+        console.log(error);
+    });
+});
 
 module.exports = router;
--- database/connection.js
+++ database/connection.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('guiapress','root','123456', {
     host: 'localhost',
     dialect: 'mysql',
+    timezone: "-03:00",
 });
 
 module.exports = connection;
